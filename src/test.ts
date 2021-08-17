ec6010a8a2540cf4f3328289959242273db14cda
Vladislav Marchenko <w3k5@yandex.ru>
2021-08-17 23:08:19 +0300

    Lint fixes

--- src/test.ts
+++ src/test.ts
@@ -2,16 +2,13 @@
 
 import 'zone.js/testing';
 import { getTestBed } from '@angular/core/testing';
-import {
-  BrowserDynamicTestingModule,
-  platformBrowserDynamicTesting,
-} from '@angular/platform-browser-dynamic/testing';
+import { BrowserDynamicTestingModule, platformBrowserDynamicTesting } from '@angular/platform-browser-dynamic/testing';
 
 declare const require: {
   context(
     path: string,
     deep?: boolean,
-    filter?: RegExp
+    filter?: RegExp,
   ): {
     keys(): string[];
     <T>(id: string): T;
@@ -19,11 +16,9 @@ declare const require: {
 };
 
 // First, initialize the Angular testing environment.
-getTestBed().initTestEnvironment(
-  BrowserDynamicTestingModule,
-  platformBrowserDynamicTesting(),
-  { teardown: { destroyAfterEach: true } }
-);
+getTestBed().initTestEnvironment(BrowserDynamicTestingModule, platformBrowserDynamicTesting(), {
+  teardown: { destroyAfterEach: true },
+});
 
 // Then we find all the tests.
 const context = require.context('./', true, /\.spec\.ts$/);
